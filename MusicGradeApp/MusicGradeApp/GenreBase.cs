d6c1cfbc93321d197afda46d5dca19f9252e3423
Gnomek1 <kacperbab@protonmail.com>
2024-02-26 23:01:37 +0100

    add interfase and base abstrackt class

---
+++ MusicGradeApp/MusicGradeApp/GenreBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicGradeApp
+{
+    public abstract class GenreBase : IGenre
+    {
+       // private List<Track> tracks;
+       // jak dodac prywatna liste do kontruktora w klasie dziedziczace (GenreInMemory)
+        public string MusicGenre { get; private set; }
+
+        public GenreBase(string musicGenre)
+        {
+            MusicGenre = musicGenre;
+        }
+
+        public abstract void AddTrack(string title, float rating);
+        public abstract void AddTrack(string title, string rating);
+        public abstract void ShowTracks();
+        public abstract Statistics GetStatistics();
+       
+    }
+}
---
+++ MusicGradeApp/MusicGradeApp/IGenre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicGradeApp
+{
+    public interface IGenre
+    {
+        string MusicGenre { get; }
+
+        void AddTrack(string title, float rating);
+        void AddTrack(string title, string rating);
+        Statistics GetStatistics();
+    }
+}
