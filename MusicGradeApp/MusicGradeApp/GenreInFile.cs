d43aa11374349df660be13391825be746445e729
Gnomek1 <kacperbab@protonmail.com>
2024-02-28 19:30:54 +0100

    add logic to Program.cs , and adjust some in file class

---
+++ MusicGradeApp/MusicGradeApp/GenreInFile.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MusicGradeApp
+{
+    public class GenreInFile : GenreBase
+    {
+        private readonly string fileName;
+        private List<Track> tracks;
+
+        public GenreInFile(string musicGenre) : base(musicGenre)
+        {
+            tracks = new List<Track>();
+            fileName = musicGenre + "rating.txt";
+        }
+
+        public override void AddTrack(string title, int rating)
+        {
+            if (rating >= 0 && rating <= 100)
+            {
+                Track track = new Track(title, rating);
+                using (var writer = File.AppendText(fileName))
+                {
+                    writer.WriteLine($"{title}    {rating}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter rate between 0 and 100");
+            }
+        }
+
+        public override void AddTrack(string title, string rating)
+        {
+            if (int.TryParse(rating, out int result))
+            {
+                AddTrack(title, result);
+            }
+            else
+            {
+                Console.WriteLine("enter valid string");
+            }
+        }
+
+        public override Statistics GetStatistics()
+        {
+            var stats = new Statistics();
+            if (File.Exists(fileName))
+            {
+                using (var reader = File.OpenText(fileName))
+                {
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        if (line.Length >= 3)
+                        {
+                            var lastThreeChars = line.Substring(line.Length - 3);
+                            lastThreeChars = lastThreeChars.Trim();
+                            if (int.TryParse(lastThreeChars, out int rating))
+                            {
+                                stats.AddGrade(rating);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Nie udało się sparsować oceny z linii: {line}");
+                            }
+                        }
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            else
+            {
+                using (var reader = File.OpenText(fileName))
+                {
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        if (line.Length >= 3)
+                        {
+                            var lastThreeChars = line.Substring(line.Length - 3);
+                            lastThreeChars = lastThreeChars.Trim();
+                            if (int.TryParse(lastThreeChars, out int rating))
+                            {
+                                stats.AddGrade(rating);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Nie udało się sparsować oceny z linii: {line}");
+                            }
+                        }
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            return stats;
+        }
+        public override void ShowTracks()
+        {
+            if (File.Exists(fileName))
+            {
+                using (var reader = File.OpenText(fileName))
+                {
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = reader.ReadLine();
+                    }
+
+                }
+            }
+        }
+    }
+}
+
--- MusicGradeApp/MusicGradeApp/Program.cs
+++ MusicGradeApp/MusicGradeApp/Program.cs
@@ -4,49 +4,109 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Welcome in Music Grades App");
-            //Console.WriteLine("============================");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome in Music Grades App");
+            Console.WriteLine("============================");
+            Console.ResetColor();
+            bool CloseApp = true;
+            string musicGenre = "";
 
-            //string musicGenre = "";
-            //while (musicGenre.Length == 0)
-            //{
-            //    Console.Write("Please enter a music genre: ");
-            //    musicGenre = Console.ReadLine();
-            //}
-            //GenreInMemory genre = new GenreInMemory(musicGenre);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("1-Add track name and rating to program memory and show stats");
+            Console.WriteLine("2-Add track name and rating to \"grenre_name\"rating.txt and show stats");
+            Console.WriteLine("X-Close App");
+            var userInput = Console.ReadLine().ToUpper();
+            Console.ResetColor();
+            while (CloseApp)
+            {
+                if (userInput == "1")
+                {
+                    while (musicGenre.Length == 0)
+                    {
+                        Console.Write("Please enter a music genre: ");
+                        musicGenre = Console.ReadLine();
+                    }
+                    GenreInMemory genre = new GenreInMemory(musicGenre);
 
-            //string track = "";
-            //string rating = "";
+                    string track = "";
+                    string rating = "";
 
-            //while (true)
-            //{
-            //    Console.Write($"Please enter a track name for {musicGenre} (or press 'q' to exit): ");
-            //    var input = Console.ReadLine();
-            //    if (input.ToLower() == "q")
-            //    {
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        track = input;
-            //        Console.Write($"Please enter a {track} rating (or press 'q' to exit): ");
-            //        input = Console.ReadLine();
-            //        rating = input;
-            //        genre.AddTrack(track, rating);
+                    while (true)
+                    {
+                        Console.Write($"Please enter a track name for {musicGenre} (or press 'q' to exit): ");
+                        var input = Console.ReadLine();
+                        if (input.ToLower() == "q")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            track = input;
+                            Console.Write($"Please enter a {track} rating (or press 'q' to exit): ");
+                            input = Console.ReadLine();
+                            rating = input;
+                            genre.AddTrack(track, rating);
 
-            //    }
-            //}
-            //genre.ShowTracks();
-            GenreInFile genre = new GenreInFile("ok");
-            genre.AddTrack("ok", 80);
+                        }
+                    }
+                    Statistics statistics = genre.GetStatistics();
+                    Console.WriteLine($"max:{statistics.Max}");
+                    Console.WriteLine($"min:{statistics.Min}");
+                    Console.WriteLine($"avg:{statistics.Average}");
+                    genre.ShowTracks();
+                    CloseApp=false;
+                }
+                else if (userInput == "2")
+                {
+                        while (musicGenre.Length == 0)
+                        {
+                            Console.Write("Please enter a music genre: ");
+                            musicGenre = Console.ReadLine();
+                        }
+                        GenreInMemory genre = new GenreInMemory(musicGenre);
 
-            Statistics statistics = new Statistics();
-            
-            Console.WriteLine($"{statistics.Max}");
-            Console.WriteLine($"{statistics.Min}");
-            Console.WriteLine($"{statistics.Average}");
+                        string track = "";
+                        string rating = "";
+
+                        while (true)
+                        {
+                            Console.Write($"Please enter a track name for {musicGenre} (or press 'q' to exit): ");
+                            var input = Console.ReadLine();
+                            if (input.ToLower() == "q")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                track = input;
+                                Console.Write($"Please enter a {track} rating (or press 'q' to exit): ");
+                                input = Console.ReadLine();
+                                rating = input;
+                                genre.AddTrack(track, rating);
+
+                            }
+                        }
+                        Statistics statistics = genre.GetStatistics();
+                        Console.WriteLine($"max:{statistics.Max}");
+                        Console.WriteLine($"min:{statistics.Min}");
+                        Console.WriteLine($"avg:{statistics.Average}");
+                        genre.ShowTracks();
+                    CloseApp = false;
+                }
+                else if (userInput == "X")
+                {
+                    Console.WriteLine("Good Bye");
+                    CloseApp = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid option");
+                    continue;
+                }
 
 
+
+            }
         }
     }
 }
